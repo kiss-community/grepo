ef6de15bc1f0a9471ece5376383ba16525734f0a
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-10-02 18:57:42 +0530

    *: bump to 7e840bb8c42bdcae51da283fe9588bf239b0c0f5

--- core/openssl/files/update-certdata.sh
+++ core/openssl/files/update-certdata.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
 
-[ -w /etc/ssl ] || {
+[ -w "$KISS_ROOT/etc/ssl" ] || {
     printf '%s\n' "${0##*/}: root required to update cert." >&2
     exit 1
 }
 
-cd /etc/ssl && {
+cd "$KISS_ROOT/etc/ssl" && {
     curl -LO https://curl.haxx.se/ca/cacert.pem
     mv -f cacert.pem cert.pem
     printf '%s\n' "${0##*/}: updated cert.pem"
